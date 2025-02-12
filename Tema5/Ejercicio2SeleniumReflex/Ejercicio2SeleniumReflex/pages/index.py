81c23a5057b9d691b169fe45e3621350ad35f77c
nestorss05 <nestor.sanchez@iesnervion.es>
2025-02-12 23:44:23 +0100

    Mas tests en el ejercicio 4

--- Tema5/Ejercicio2SeleniumReflex/Ejercicio2SeleniumReflex/pages/index.py
+++ Tema5/Ejercicio2SeleniumReflex/Ejercicio2SeleniumReflex/pages/index.py
@@ -1,14 +1,32 @@
+from threading import Event
+
 import reflex as rx
 
 from Ejercicio2SeleniumReflex.routes import Ruta
 
 class FormState(rx.State):
     form_data: dict = {},
+    nombre = ""
+    apellidos = ""
+    correo = ""
+    novs: bool = True
 
     @rx.event
     def handle_submit(self, form_data: dict):
         """Handle the form submit."""
         self.form_data = form_data
+
+    def setNombre(self, nombre: str):
+        self.nombre = nombre
+
+    def setApellidos(self, apellidos: str):
+        self.apellidos = apellidos
+
+    def setCorreo(self, correo: str):
+        self.correo = correo
+
+    def setNovs(self, novs: bool):
+        self.novs = novs
 
 @rx.page(route=Ruta.INDEX.value, title="Formulario de registro - Mi web")
 def index() -> rx.Component:
@@ -16,10 +34,50 @@ def index() -> rx.Component:
         rx.heading("Formulario de registro", id="heading"),
         rx.form(
             rx.vstack(
-                rx.input(
-                    placeholder="Nombre",
-                    name="nombre",
-                    id="nombre",
+                rx.hstack(
+                    rx.text("Nombre: "),
+                    rx.input(
+                        placeholder="Nombre",
+                        value=FormState.nombre,
+                        on_change=FormState.setNombre,
+                        id="nombre",
+                        type="text",
+                        max_length=50,
+                    ),
+                ),
+                rx.hstack(
+                    rx.text("Apellidos: "),
+                    rx.input(
+                        placeholder="Apellidos",
+                        value=FormState.apellidos,
+                        on_change=FormState.setApellidos,
+                        id="apellidos",
+                        type="text",
+                        max_length=50,
+                    ),
+                ),
+                rx.hstack(
+                    rx.text("Correo: "),
+                    rx.input(
+                        placeholder="Correo",
+                        value=FormState.correo,
+                        on_change=FormState.setCorreo,
+                        id="correo",
+                        type="text",
+                    ),
+                ),
+                rx.hstack(
+                    rx.checkbox(
+                        on_change=FormState.setNovs,
+                        checked=FormState.novs
+                    ),
+                    rx.text("Deseo recibir informacion sobre novedades y ofertas: "),
+                    id="novs",
+                ),
+                rx.button(
+                    rx.text("Enviar"),
+                    type="submit",
+                    id="enviar",
                 ),
             ),
             on_submit=FormState.handle_submit,
--- Tema5_Java/PruebasSelenium/src/ejercicio2/Ejercicio2Test.java
+++ Tema5_Java/PruebasSelenium/src/ejercicio2/Ejercicio2Test.java
@@ -2,12 +2,16 @@ package ejercicio2;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import java.time.Duration;
+
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 class Ejercicio2Test {
 
@@ -21,21 +25,127 @@ class Ejercicio2Test {
 	
 	@Test
 	void testHeader() {
+		driver1.manage().timeouts().implicitlyWait(Duration.ofSeconds(2));
 		WebElement heading = driver1.findElement(By.id("heading"));
         String texto = heading.getText();
+        driver1.manage().timeouts().implicitlyWait(Duration.ofSeconds(2));
         assertEquals("Formulario de registro", texto);
 	}
 	
 	@Test
 	void testTituloPagina() {
+		driver1.manage().timeouts().implicitlyWait(Duration.ofSeconds(2));
 		String title = driver1.getTitle();
         assertEquals("Formulario de registro - Mi web", title);
 	}
 	
 	@Test
 	void testNombreForm() {
+		driver1.manage().timeouts().implicitlyWait(Duration.ofSeconds(2));
 		WebElement nombre = driver1.findElement(By.id("nombre"));
 		assertNotNull(nombre);
+	}
+	
+	@Test
+	void testApellidosForm() {
+		driver1.manage().timeouts().implicitlyWait(Duration.ofSeconds(2));
+		WebElement apellidos = driver1.findElement(By.id("apellidos"));
+		assertNotNull(apellidos);
+	}
+	
+	@Test
+	void testNombre49() {
+		driver1.manage().timeouts().implicitlyWait(Duration.ofSeconds(2));
+		WebElement nombre = driver1.findElement(By.id("nombre"));
+		nombre.clear();
+		String nombreOG = "Gustavo Aurelio Jocantaro Paulino de las Nieves P";
+		nombre.sendKeys(nombreOG);
+		driver1.manage().timeouts().implicitlyWait(Duration.ofSeconds(2));
+		assertEquals(nombre.getAttribute("value"), nombreOG);
+	}
+	
+	@Test
+	void testNombre50() {
+		driver1.manage().timeouts().implicitlyWait(Duration.ofSeconds(2));
+		WebElement nombre = driver1.findElement(By.id("nombre"));
+		nombre.clear();
+		String nombreOG = "Gustavo Aurelio Jocantaro Paulino de las Nieves Pe";
+		nombre.sendKeys(nombreOG);
+		driver1.manage().timeouts().implicitlyWait(Duration.ofSeconds(2));
+		assertEquals(nombre.getAttribute("value"), nombreOG);
+	}
+	
+	@Test
+	void testNombre51() {
+		driver1.manage().timeouts().implicitlyWait(Duration.ofSeconds(2));
+		WebElement nombre = driver1.findElement(By.id("nombre"));
+		nombre.clear();
+		String nombreOG = "Gustavo Aurelio Jocantaro Paulino de las Nieves Pez";
+		nombre.sendKeys(nombreOG);
+		driver1.manage().timeouts().implicitlyWait(Duration.ofSeconds(2));
+		assertNotEquals(nombre.getAttribute("value"), nombreOG);		
+	}
+	
+	@Test
+	void testApellidos49() {
+		driver1.manage().timeouts().implicitlyWait(Duration.ofSeconds(2));
+		WebElement apellidos = driver1.findElement(By.id("apellidos"));
+		apellidos.clear();
+		String apellidoOG = "Cuevas Sanchez Gonzalez Ordoñez Cruz Rivas Hernan";
+		apellidos.sendKeys(apellidoOG);
+		driver1.manage().timeouts().implicitlyWait(Duration.ofSeconds(2));
+		assertEquals(apellidos.getAttribute("value"), apellidoOG);
+	}
+	
+	@Test
+	void testApellidos50() {
+		driver1.manage().timeouts().implicitlyWait(Duration.ofSeconds(2));
+		WebElement apellidos = driver1.findElement(By.id("apellidos"));
+		apellidos.clear();
+		String apellidoOG = "Cuevas Sanchez Gonzalez Ordoñez Cruz Rivas Hernand";
+		apellidos.sendKeys(apellidoOG);
+		driver1.manage().timeouts().implicitlyWait(Duration.ofSeconds(2));
+		assertEquals(apellidos.getAttribute("value"), apellidoOG);
+	}
+	
+	@Test
+	void testApellidos51() {
+		driver1.manage().timeouts().implicitlyWait(Duration.ofSeconds(2));
+		WebElement apellidos = driver1.findElement(By.id("apellidos"));
+		apellidos.clear();
+		String apellidoOG = "Cuevas Sanchez Gonzalez Ordoñez Cruz Rivas Hernando";
+		apellidos.sendKeys(apellidoOG);
+		driver1.manage().timeouts().implicitlyWait(Duration.ofSeconds(2));
+		assertNotEquals(apellidos.getAttribute("value"), apellidoOG);
+	}
+	
+	@Test
+	void selectCorreo() {
+		driver1.manage().timeouts().implicitlyWait(Duration.ofSeconds(2));
+		WebElement correo = driver1.findElement(By.id("correo"));
+		assertNotNull(correo);
+	}
+	
+	@Test
+	void selectSubmit() {
+		driver1.manage().timeouts().implicitlyWait(Duration.ofSeconds(2));
+		WebElement enviar = driver1.findElement(By.id("enviar"));
+		assertNotNull(enviar);
+	}
+	
+	@Test
+	void selectCheckbox1() {
+		driver1.manage().timeouts().implicitlyWait(Duration.ofSeconds(2));
+		WebElement novs = driver1.findElement(By.id("novs"));
+		assertNotNull(novs);
+	}
+	
+	@Test
+	void checkCheckbox1() {
+		driver1.manage().timeouts().implicitlyWait(Duration.ofSeconds(2));
+		WebElement novs = driver1.findElement(By.id("novs"));
+		System.out.println(novs.isSelected());
+		assertTrue(novs.isSelected());
 	}
 
 }
